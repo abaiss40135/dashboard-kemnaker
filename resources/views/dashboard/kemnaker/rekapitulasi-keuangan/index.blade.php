106a239ba05fc4ace33c47872442e3f40968b207
Aziz <azizrn38@gmail.com>
2024-12-24 16:36:47 +0700

    update recaps style

--- resources/views/dashboard/kemnaker/rekapitulasi-keuangan/index.blade.php
+++ resources/views/dashboard/kemnaker/rekapitulasi-keuangan/index.blade.php
@@ -7,21 +7,19 @@
 @endsection
 
 @section('content')
-    <div class="grid grid-cols-1 gap-x-5">
-        <div class="box">
-            <div class="box-header items-center border-0">
-                <h2 class="mt-0">
-                    Sistem pencatatan dan monitoring capaian atas pelaksanaan program dan kegiatan Kementerian Ketenagakerjaan
-                </h2>
-            </div>
+    <div class="box">
+        <div class="box-header items-center border-0">
+            <h2 class="mt-0">
+                Sistem pencatatan dan monitoring capaian atas pelaksanaan program dan kegiatan Kementerian Ketenagakerjaan
+            </h2>
         </div>
-
-        @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.recap-links')
-        @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.filters')
-        @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.recaps')
-        @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.chart-keuangan')
-        @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.realisasi-anggaran')
-        @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.pelaksanaan-anggaran')
-        @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.realisasi-pengadaan')
     </div>
+
+    @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.recap-links')
+    @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.filters')
+    @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.recaps')
+    @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.chart-keuangan')
+    @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.realisasi-anggaran')
+    @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.pelaksanaan-anggaran')
+    @include('dashboard.kemnaker.rekapitulasi-keuangan.partials.realisasi-pengadaan')
 @endsection
--- resources/views/dashboard/kemnaker/rekapitulasi-keuangan/partials/recap-links.blade.php
+++ resources/views/dashboard/kemnaker/rekapitulasi-keuangan/partials/recap-links.blade.php
@@ -1,36 +1,50 @@
-<div class="box-body pt-0 px-0 summery-box">
-    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
-        <a href="#" class="col-lg-3 col-md-6">
-            <div class="box border border-primary rounded-2xl pull-up py-10 h-full">
-                <div class="box-body ">
-                    <i class="fas fa-chart-bar mb-8 text-8xl"></i>
-                    <h2 class="fw-600 mb-0 text-2xl">Rekap Realisasi Belanja Per Jenis Belanja</h2>
+<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-x-4">
+    <div class="box pull-up">
+        <div class="box-body py-12">
+            <div class="flex justify-between items-center">
+                <div class="bs-5 ps-10 border-primary">
+                    <h2 class="my-0 fw-700 text-2xl text-dark">Rekap Realisasi Belanja Per Jenis Belanja</h2>
+                </div>
+                <div class="icon">
+                    <i class="fa-solid fa-chart-bar bg-primary-light me-0 fs-24 rounded-3"></i>
                 </div>
             </div>
-        </a>
-        <a href="#" class="col-lg-3 col-md-6">
-            <div class="box border border-primary rounded-2xl pull-up py-10 h-full">
-                <div class="box-body ">
-                    <i class="fas fa-chart-area mb-8 text-8xl"></i>
-                    <h2 class="fw-600 mb-0 text-2xl">Rekap Realisasi Belanja Per Wilayah</h2>
+        </div>
+    </div>
+    <div class="box pull-up">
+        <div class="box-body py-12">
+            <div class="flex justify-between items-center">
+                <div class="bs-5 ps-10 border-info">
+                    <h2 class="my-0 fw-700 text-2xl text-dark">Rekap Realisasi Belanja Per Wilayah</h2>
+                </div>
+                <div class="icon">
+                    <i class="fa-solid fa-chart-area bg-info-light me-0 fs-24 rounded-3"></i>
                 </div>
             </div>
-        </a>
-        <a href="#" class="col-lg-3 col-md-6">
-            <div class="box border border-primary rounded-2xl pull-up py-10 h-full">
-                <div class="box-body ">
-                    <i class="fas fa-chart-line mb-8 text-8xl"></i>
-                    <h2 class="fw-600 mb-0 text-2xl">Rekap Realisasi Belanja Per Sumber Dana</h2>
+        </div>
+    </div>
+    <div class="box pull-up">
+        <div class="box-body py-12">
+            <div class="flex justify-between items-center">
+                <div class="bs-5 ps-10 border-warning">
+                    <h2 class="my-0 fw-700 text-2xl text-dark">Rekap Realisasi Belanja Per Sumber Dana</h2>
+                </div>
+                <div class="icon">
+                    <i class="fa-solid fa-chart-line bg-warning-light me-0 fs-24 rounded-3"></i>
                 </div>
             </div>
-        </a>
-        <a href="#" class="col-lg-3 col-md-6">
-            <div class="box border border-primary rounded-2xl pull-up py-10 h-full">
-                <div class="box-body ">
-                    <i class="fas fa-chart-pie mb-8 text-8xl"></i>
-                    <h2 class="fw-600 mb-0 text-2xl">Capaian IKPA Kemnaker</h2>
+        </div>
+    </div>
+    <div class="box pull-up">
+        <div class="box-body py-12">
+            <div class="flex justify-between items-center">
+                <div class="bs-5 ps-10 border-danger">
+                    <h2 class="my-0 fw-700 text-2xl text-dark">Capaian IKPA Kemnaker</h2>
+                </div>
+                <div class="icon">
+                    <i class="fa-solid fa-chart-pie bg-danger-light me-0 fs-24 rounded-3"></i>
                 </div>
             </div>
-        </a>
+        </div>
     </div>
 </div>
