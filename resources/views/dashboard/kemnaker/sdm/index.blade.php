cbe99e47b88c55e153fd79e7d7f45aa48b237efc
aziz <aziz@brainmatics.com>
2024-12-30 11:38:32 +0700

    add(kemnaker/sdm): add tinkat kehadiran block

--- resources/views/dashboard/kemnaker/sdm/index.blade.php
+++ resources/views/dashboard/kemnaker/sdm/index.blade.php
@@ -4,4 +4,5 @@
     @include('dashboard.kemnaker.sdm.partials.recap-links')
     @include('dashboard.kemnaker.sdm.partials.filters')
     @include('dashboard.kemnaker.sdm.partials.recaps')
+    @include('dashboard.kemnaker.sdm.partials.tingkat-kehadiran')
 @endsection
